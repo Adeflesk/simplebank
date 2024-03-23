4659c2538e4856cc82c865b894fb1d794507b21a
Adrian Corsini <glenflesk@gmail.com>
2024-03-23 11:11:31 +0000

    creating testing of CRUD

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,62 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"example.com/db/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+	args := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomMoney(),
+	}
+	transfer, err := testQueries.CreateTransfer(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, transfer.FromAccountID, account1.ID)
+	require.Equal(t, transfer.ToAccountID, account2.ID)
+	require.Equal(t, transfer.Amount, args.Amount)
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer := createRandomTransfer(t, account1, account2)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer2.ID, transfer.ID)
+	require.Equal(t, transfer2.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, transfer2.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, transfer2.Amount, transfer.Amount)
+
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, account1, account2)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer2.ID, transfer1.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+
+}
